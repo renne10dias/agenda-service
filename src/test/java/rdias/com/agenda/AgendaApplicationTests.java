8ea8004803b15259d1e9928b85fe8b48716ab7cd
renne farias dias <rennedias2003@gmail.com>
2023-12-01 16:58:00 -0300

    Teste

---
+++ src/test/java/rdias/com/agenda/AgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package rdias.com.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
