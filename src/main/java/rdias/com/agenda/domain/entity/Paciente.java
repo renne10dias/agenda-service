6663a961efdbbd7c6d17a8850b0c011e07ae3a42
renne farias dias <rennedias2003@gmail.com>
2023-12-02 01:22:25 -0300

    Configuração de ambiente e Entidades

---
+++ src/main/java/rdias/com/agenda/domain/entity/Paciente.java
@@ -0,0 +1,21 @@
+package rdias.com.agenda.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "paciente")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String sobrenome;
+    private String email;
+    private String cpf;
+
+}
